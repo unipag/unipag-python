baaaef853df2f4f12b5793b80cf525d75274c43b
Denis Stebunov <denis.stebunov@ivelum.com>
2013-08-27 09:02:55 +0400

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -16,23 +16,24 @@ requires = ['requests>=0.9']
 setup(
     name='unipag',
     version=unipag.__version__,
-    description='Unipag.py - Unipag Client for Python',
+    description='Unipag Client for Python',
     long_description=open('README.rst').read(),
     author='Denis Stebunov',
-    author_email='denis.stebunov@ivelum.com',
-    url='https://github.com/ivelum/unipag.py',
+    author_email='support@unipag.com',
+    url='https://github.com/unipag/unipag-python',
     packages=packages,
     install_requires=requires,
     license=open('LICENSE').read(),
     zip_safe=False,
     classifiers=(
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        ),
+    ),
 )
 
 del os.environ['PYTHONDONTWRITEBYTECODE']
